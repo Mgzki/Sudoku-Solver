e361badc06e9da4ed0be4937086d9b6b930ca882
Max <maxgrodzki@gmail.com>
2020-07-28 20:04:58 -0400

    Various modifications/additions
    Methods : convert Board to 2DArray, checking for invalid rows/columns
    Modifications: updated comments, changed "empty/reset" value to 0 from ' '

--- src/Board.java
+++ src/Board.java
@@ -1,14 +1,16 @@
-public class Board {
-    private final int[][] board; // 2D array of chars
+import com.sun.rowset.internal.Row;
 
-    //Constructs the 3 x 3 grid with enumerated empty cells for the playing board
+public class Board {
+    private final int[][] board; // 2D array of ints
+
+    //Constructs the 9 x 9 grid with empty cells detonated by 0
     public Board() {
         board = new int[9][9];
         for (int row = 0; row < 9; row++)
             for (int col = 0; col < 9; col++)
                 board[row][col] = 0;
     }
-    //Sets the players move in the selected cells
+    //Sets the players move in the selected cell
     public boolean set_Move(int[] move, int number) {
         if (cell_Free(move)) {
             board[move[0]][move[1]] = number;
@@ -18,7 +20,7 @@ public class Board {
         }
     }
     public void reset_Move(int[]move){
-        board[move[0]][move[1]] = ' ';
+        board[move[0]][move[1]] = 0;
     }
     //Gets the current value from a position on the board
     public int get_Move(int [] move){
@@ -26,7 +28,7 @@ public class Board {
     }
     //Checks if the cell is free
     public boolean cell_Free(int[] move){
-        return (board[move[0]][move[1]] == ' ');
+        return (board[move[0]][move[1]] == 0);
     }
     /*Checks if there are any available moves
      *Used for tie checking in Tic Tac Toe
@@ -57,7 +59,7 @@ public class Board {
                     System.out.println("\n-----|-----|-----");
             }else
                 System.out.println();
-        }System.out.println();
+        }System.out.println("#################");
     }
     //add actual checking for if all the digits are in the row
     public boolean check_Row(int row, char player){
@@ -69,9 +71,16 @@ public class Board {
     //add actual checking for if all the digits are in the column
     public boolean check_Col(int col){
         for (int row = 0; row < 9; row++){
-            if (board[row][col] == ' ')
+            if (board[row][col] == 0)
                 return false; //cell didn't match
         }return true; // full col
+    }
+    public int[][] to2DArray() {
+        int[][] copyOfBoard = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++)
+                copyOfBoard[i][j] = board[i][j];
+        }return copyOfBoard;
     }
 }
 
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,6 +1,63 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Sudoku {
+    public static boolean rowsInvalid(Board game){
+        for (int i = 0; i < 9; i++){
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < 9; j++) {
+                row.add(game.to2DArray()[i][j]);
+            } if (hasRepeats(row)) return true;
+        } return false;
+    }
+
+    public static boolean columnsInvalid(Board game){
+        for (int i = 0; i < 9; i++){
+            List<Integer> col = new ArrayList<>();
+            for (int j = 0; j < 9; j++) {
+                col.add(game.to2DArray()[j][i]);
+            } if (hasRepeats(col)) return true;
+        } return false;
+    }
+
+    public static boolean hasRepeats(List<Integer> collection){
+        for (int index = 1; index <= 9; index++) {
+            //System.out.print(Collections.frequency(collection, index));
+            if (Collections.frequency(collection, index) > 1)
+                return true;
+        }
+        System.out.println();
+        return false;
+    }
+
     public static void main(String[] args){
         Board game = new Board();
         game.display();
+        ///*
+        //filled column testing
+        game.set_Move(new int[] {0,0}, 1);
+        game.set_Move(new int[] {1,0}, 1);
+        game.set_Move(new int[] {2,0}, 3);
+        game.set_Move(new int[] {3,0}, 4);
+        game.set_Move(new int[] {4,0}, 5);
+        game.set_Move(new int[] {5,0}, 6);
+        game.set_Move(new int[] {6,0}, 7);
+        game.set_Move(new int[] {7,0}, 8);
+        game.set_Move(new int[] {8,0}, 9);
+
+        //filled row testing
+        game.set_Move(new int[] {0,1}, 2);
+        game.set_Move(new int[] {0,2}, 2);
+        game.set_Move(new int[] {0,3}, 4);
+        game.set_Move(new int[] {0,4}, 5);
+        game.set_Move(new int[] {0,5}, 6);
+        game.set_Move(new int[] {0,6}, 7);
+        game.set_Move(new int[] {0,7}, 8);
+        game.set_Move(new int[] {0,8}, 9);
+         //*/
+
+        columnsInvalid(game);
+        rowsInvalid(game);
     }
 }
