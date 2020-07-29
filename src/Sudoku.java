06d0cceb7efc973582b89903c95e8e588e850a84
Max <maxgrodzki@gmail.com>
2020-07-29 15:44:47 -0400

    Added checking for a completed game

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -11,7 +11,6 @@ public class Sudoku {
             } if (hasRepeats(row)) return true;
         } return false;
     }
-
     public static boolean columnsInvalid(Board game){
         for (int i = 0; i < 9; i++){
             List<Integer> col = new ArrayList<>();
@@ -20,21 +19,70 @@ public class Sudoku {
             } if (hasRepeats(col)) return true;
         } return false;
     }
-
+    public static boolean squareInvalid(Board game, int x, int y){
+        int xEnd = x + 3;
+        int yEnd = y + 3;
+        List<Integer> square = new ArrayList<>();
+        for (int i = x; i < xEnd; i++) {
+            for (int j = y; j < yEnd; j++)
+                square.add(game.to2DArray()[i][j]);
+        } if (hasRepeats(square)) return true;
+        return false;
+    }
+    public static boolean rowSquareInvalid(Board game, int row){
+        switch (row){
+            case 0: //first row of squares
+                if (squareInvalid(game,0,0)) return true;
+                if (squareInvalid(game,0,3)) return true;
+                if (squareInvalid(game,0,6)) return true;
+            case 1: //second row of squares
+                if (squareInvalid(game,3,0)) return true;
+                if (squareInvalid(game,3,3)) return true;
+                if (squareInvalid(game,3,6)) return true;
+            case 2: //third row of squares
+                if (squareInvalid(game,6,0)) return true;
+                if (squareInvalid(game,6,3)) return true;
+                if (squareInvalid(game,6,6)) return true;
+            default: return false;
+        }
+    }
+    public static boolean allSquareInvalid(Board game){
+        if (rowSquareInvalid(game, 0)) return true; // first row of squares
+        if (rowSquareInvalid(game, 1)) return true; // second row of squares
+        if (rowSquareInvalid(game, 2)) return true; // third row of squares
+        return false;
+    }
     public static boolean hasRepeats(List<Integer> collection){
         for (int index = 1; index <= 9; index++) {
-            //System.out.print(Collections.frequency(collection, index));
             if (Collections.frequency(collection, index) > 1)
                 return true;
         }
-        System.out.println();
         return false;
+    }
+    /*
+     * Checks if any sudoku conditions have been broken (
+     */
+    public static boolean sudokuInvalid(Board game){
+        if (rowsInvalid(game)) return true;
+        if (columnsInvalid(game)) return true;
+        if (allSquareInvalid(game)) return true;
+        else return false;
+    }
+    /*
+     * If either a sudoku condition is broken(invalid) or there are no free moves
+     * then the game is incomplete. Once all sudoku conditions are met and there
+     * are no free moves, the sudoku is complete.
+     */
+    public static boolean sudokuCompleted(Board game){
+        if (sudokuInvalid(game)) return false;
+        if (game.full_Board()) return false;
+        return true;
     }
 
     public static void main(String[] args){
         Board game = new Board();
         game.display();
-        ///*
+        /*
         //filled column testing
         game.set_Move(new int[] {0,0}, 1);
         game.set_Move(new int[] {1,0}, 1);
@@ -56,8 +104,7 @@ public class Sudoku {
         game.set_Move(new int[] {0,7}, 8);
         game.set_Move(new int[] {0,8}, 9);
          //*/
-
-        columnsInvalid(game);
-        rowsInvalid(game);
+        //game.display();
+        System.out.print(sudokuInvalid(game));
     }
 }
