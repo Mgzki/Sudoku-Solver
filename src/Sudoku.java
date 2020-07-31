99b03a1fd87efbf825dd459fc34db088d8d68b60
Max <maxgrodzki@gmail.com>
2020-07-31 15:22:57 -0400

    Remove obsolete code
    Removed alternate methods for Sudoku condition checking

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -3,92 +3,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class Sudoku {
-    /*
-     * Block of alternate sudoku condition checking methods.
-
-    public static boolean rowsInvalid(Board game){
-        for (int i = 0; i < 9; i++){
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < 9; j++) {
-                row.add(game.to2DArray()[i][j]);
-            } if (hasRepeats(row)) return true;
-        } return false;
-    }
-
-    public static boolean columnsInvalid(Board game){
-        for (int i = 0; i < 9; i++){
-            List<Integer> col = new ArrayList<>();
-            for (int j = 0; j < 9; j++) {
-                col.add(game.to2DArray()[j][i]);
-            } if (hasRepeats(col)) return true;
-        } return false;
-    }
-
-    public static boolean squareInvalid(Board game, int x, int y){
-        int xEnd = x + 3;
-        int yEnd = y + 3;
-        List<Integer> square = new ArrayList<>();
-        for (int i = x; i < xEnd; i++) {
-            for (int j = y; j < yEnd; j++)
-                square.add(game.to2DArray()[i][j]);
-        } if (hasRepeats(square)) return true;
-        return false;
-    }
-
-    public static boolean rowSquareInvalid(Board game, int row){
-        switch (row){
-            case 0: //first row of squares
-                if (squareInvalid(game,0,0)) return true;
-                if (squareInvalid(game,0,3)) return true;
-                if (squareInvalid(game,0,6)) return true;
-            case 1: //second row of squares
-                if (squareInvalid(game,3,0)) return true;
-                if (squareInvalid(game,3,3)) return true;
-                if (squareInvalid(game,3,6)) return true;
-            case 2: //third row of squares
-                if (squareInvalid(game,6,0)) return true;
-                if (squareInvalid(game,6,3)) return true;
-                if (squareInvalid(game,6,6)) return true;
-            default: return false;
-        }
-    }
-
-    public static boolean allSquareInvalid(Board game){
-        if (rowSquareInvalid(game, 0)) return true; // first row of squares
-        if (rowSquareInvalid(game, 1)) return true; // second row of squares
-        if (rowSquareInvalid(game, 2)) return true; // third row of squares
-        return false;
-    }
-    // Checks for repeats within the inputted array of 9 values (row, col, or square)
-    public static boolean hasRepeats(List<Integer> collection){
-        for (int index = 1; index <= 9; index++) {
-            if (Collections.frequency(collection, index) > 1)
-                return true;
-        }
-        return false;
-    }
-
-    // Checks if any sudoku conditions have been broken
-    public static boolean sudokuInvalid(Board game){
-        if (rowsInvalid(game)) return true;
-        if (columnsInvalid(game)) return true;
-        if (allSquareInvalid(game)) return true;
-        else return false;
-    }
-
-    /*
-     * If either a sudoku condition is broken(invalid) or there are no free moves
-     * then the game is incomplete. Once all sudoku conditions are met and there
-     * are no free moves, the sudoku is complete.
-     *\/
-
-    public static boolean sudokuCompleted(Board game){
-        if (sudokuInvalid(game)) return false;
-        if (game.full_Board()) return false;
-        return true;
-    }
-    */
-
     // Returns position of first empty cell if one is found, otherwise return invalid move
     public static int[] findEmpty(Board game){
         int[] move = new int[2];
@@ -164,7 +78,7 @@ public class Sudoku {
         if (inputBoard.length == 9) {
             game = new Board(inputBoard);
         }
-        
+
         game.display(); //Displays the board prior to being solved
         solve(game);
         game.display(); //Displays the board after being solved
